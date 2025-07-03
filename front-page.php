d5d6e027e6a3cda46b8eee29e55dc1fa867ac68b
Ivana <ivana.helvin@gmail.com>
2025-07-03 15:02:25 +0000

    homepage on progress grid box 1

--- front-page.php
+++ front-page.php
@@ -1,13 +1,70 @@
 <?php get_header(); ?>
 
-<main id="main" class="site-main">
+<!-- <main id="main" class="site-main">
    <?php
-   if ( have_posts() ) :
+   /* if ( have_posts() ) :
        while ( have_posts() ) : the_post();
            the_content();
        endwhile;
-   endif;
+   endif; */
    ?>
+</main> -->
+
+<main id="main" class="site-main">
+    <div class="homepage-container-1">
+        
+        <div class="homepage-container-1-inner">
+            <div class="homepage-header-image">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/public/external/header-image.png" />
+            </div>
+            
+            <span class="homepage-header1">Tired of ineffective real estate campaigns? we've been there too</span>
+            <div class="homepage-header-title-group">
+                <div class="homepage-header-title">
+                    <span class="homepage-header-title1">Premium,</span>                        
+                    <span class="homepage-header-title2">cost-effective marketing</span>                      
+                </div>
+                <span class="homepage-header-title3">direct to sellers.</span>
+            </div>
+            <span class="homepage-header-title4">We save your budget and your bandwidth with premium direct mail that drives immediate response.</span>
+            
+            <div class="homepage-header-button">
+                <a href="#">See Mail Options</a>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/public/external/Vector.svg" />
+            </div>        
+        </div>
+
+
+
+
+
+        <div class="homepage-grid-header">
+            <div class="header-gird-box">
+                <div class="login-asset13"></div>
+    
+                <div class="login-croped-footer">
+                    <img decoding="async" alt="FooterLogoImage6672" src="<?php echo get_stylesheet_directory_uri(); ?>/public/external/croped.svg" class="login-footer-croped-image">
+                </div>
+            </div>
+
+            <div class="header-gird-box">
+                <div class="login-asset13"></div>
+    
+                <div class="login-croped-footer">
+                    <img decoding="async" alt="FooterLogoImage6672" src="<?php echo get_stylesheet_directory_uri(); ?>/public/external/croped.svg" class="login-footer-croped-image">
+                </div>
+            </div>
+
+            <div class="header-gird-box">
+                <div class="login-asset13"></div>
+    
+                <div class="login-croped-footer">
+                    <img decoding="async" alt="FooterLogoImage6672" src="<?php echo get_stylesheet_directory_uri(); ?>/public/external/croped.svg" class="login-footer-croped-image">
+                </div>
+            </div>
+        </div>
+
+    </div>
 </main>
 
 <?php get_footer(); ?>
