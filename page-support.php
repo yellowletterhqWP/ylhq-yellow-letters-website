4c6d9461a3d079bf4e1457315e5cf33f6df0b3e8
Ivana <ivana.helvin@gmail.com>
2025-07-21 11:39:16 +0000

    support page

---
+++ page-support.php
@@ -0,0 +1,67 @@
+<?php
+/* Template Name: Support */
+get_header();
+?>
+
+<main id="main" class="site-main">
+    <div class="register-hero-background"></div>
+
+    <div class="my-orders-page-wrapper global-page-wrapper">        
+        
+        <img
+        alt="RightImage6672"
+        src="<?php echo get_stylesheet_directory_uri(); ?>/public/external/rightimage6672-cck6-300w.png"
+        class="register-right-image"
+        />
+        <img
+        alt="LeftImage6672"
+        src="<?php echo get_stylesheet_directory_uri(); ?>/public/external/leftimage6672-vfcz-300w.png"
+        class="register-left-image"
+        />
+
+        <div class="letters-iteration-page-header-title-container">
+            <span class="handwritten-page-header-title1">My Account</span>
+            <span class="handwritten-page-header-title3">Where Your Information and Orders Meet.</span>
+        </div>
+
+        <div class="my-order-page-content">
+            <?php
+                get_template_part('template-parts/form-elements/breadcrumb', null, [
+                'items' => [
+                    ['label' => 'My Account', 'url' => '/my-account'],
+                    ['label' => 'Support']
+                ]
+                ]);
+            ?>
+
+            <span class="my-account-content-title">Support</span>
+
+            <?php
+                get_template_part( 'template-parts/form-elements/button', null, array(
+                    'icon' => get_stylesheet_directory_uri() . '/public/external/logout.svg',
+                    'title' => 'Logout',
+                ) );
+            ?>
+        </div>
+
+        <div class="my-order-hr-1">
+        <hr />
+
+        <?php
+            get_template_part('template-parts/form-elements/inputbox', null, [
+                'id' => 'coupon',
+                'label' => '',
+                'placeholder' => '	Search by Order ID/Name',
+                'required' => false,
+                'icon' => get_stylesheet_directory_uri() . '/public/external/search-icon.svg',
+            ]);
+        ?>
+
+        <span class="my-order-no-order">
+            Your orders will appear here once placed
+        </span>
+
+    </div>
+</main>
+
+<?php get_footer(); ?>
